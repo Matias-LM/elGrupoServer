95256fa1580b38941bb3b4950fd69bc51f872362
MatiasLopezMonteiro <50429856+MatiasLopezMonteiro@users.noreply.github.com>
2019-11-24 18:24:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-var webSocketServer = require('websocket').server;
-var http = require('http');
-const express = require('express')
-const cors = require('cors')
-
-const app = express()
-const port = process.env.PORT || 3000
+var WebSocket = require('ws');
+var https = require('https');
+var fs = require('fs');
+//const express = require('express')
+//const cors = require('cors')
+//const app = express()
 const port = process.env.PORT || 4000
-
 // list of currently connected clients (users)
 var clients = [ ];
 var playerId = null;
 var playerCount = 0;
-
-app.use(cors())
-
+//app.use(cors())
 /**
  * Helper function for escaping input strings
  */
@@ -22,82 +18,42 @@ function htmlEntities(str) {
         .replace(/&/g, '&amp;').replace(/</g, '&lt;')
         .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
   }
-
 const waitMatch = async function(){
-
     setTimeout(function(){
-
         console.log('yo');
         return "version 1";
-
     }, 2000);
     console.log('ayo');
-
 }
-
-const find = async function(){
-
-    var resp = await waitMatch();
-    return resp;
-
-}
-
-app.get('/api', (req, res) => {
-  res.status(200).json({api: 'version 1'})
-
-    var myId = playerCount;
-    playerCount++;
-    playerId = myId;
-    do{
-
-        console.log(playerId);
-
-    }while(playerId == myId);
-    console.log('Si');
-    res.status(200).json({api: myId});
-
-})
-
-app.listen(port, () => console.log('server started on port', port))
-var server = http.createServer(function(request, response) {
-    // Not important for us. We're writing WebSocket server,
-    // not HTTP server
-});
-
-var wsServer = new webSocketServer({
-    // WebSocket server is tied to a HTTP server. WebSocket
-    // request is just an enhanced HTTP request. For more info 
-    // http://tools.ietf.org/html/rfc6455#page-6
-    httpServer: server
+const server = https.createServer({
+    cert: fs.readFileSync('./cert.pem'),
+    key: fs.readFileSync('./key.pem')
   });
+const wss = new WebSocket.Server({ server });
+//funk?
+wss.on('connection', function(ws) {
 
-server.listen(port, function() {
-
-    console.log((new Date()) + " Server is listening on port "
-    + port);
-
-});
-
-wsServer.on('request', function(request) {
-
-    console.log((new Date()) + ' Connection from origin '
+    console.log('new client')
+    console.log('new client');
+    ws.send('something');
+    /*console.log((new Date()) + ' Connection from origin '
         + request.origin + '.');
-
     var connection = request.accept(null, request.origin); 
     // we need to know client index to remove them on 'close' event
     var index = clients.push(connection) - 1;
-
     for (var i=0; i < clients.length; i++) {
         console.log('estoy')
         clients[i].sendUTF(JSON.stringify({lol: 'client connected'}));
     }
     //app.listen(port, () => console.log('server started on port', port))
     connection.on('close', function(connection) {
-
         console.log((new Date()) + " Peer "
             + connection.remoteAddress + " disconnected.");      // remove user from the list of connected clients
         clients.splice(index, 1);
-
-    });
-
+    });*/
+    
+});
+server.listen(port, function() {
+    console.log((new Date()) + " Server is listening on port "
+    + port);
 });
